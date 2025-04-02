100d52416cf61633e2897d2bcc4d2f28fae05233
EZEKIEL <zablonzekky@gmail.com>
2025-04-02 16:48:30 +0300

    fixed bugs and added new files

---
+++ Model.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+# Define the User model in a separate module
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
